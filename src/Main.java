9b6418bbd6f2947a7b183d076052c70d2f1fd97a
mihaivaidos <mihai@vaidos.com>
2024-10-12 13:17:12 +0300

    Tested the first exercise methods in main.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Aufgabe_1.UniversityNotes;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Erste Aufgabe
+        int[] noten = {29, 37, 38, 41, 84, 67, 93, 100};
+        System.out.println("Nicht ausreichende Noten: " + UniversityNotes.nichtAusreichendeNoten(noten));
+        System.out.println("Durchschnittswert: " + UniversityNotes.durchschnittsWert(noten));
+        System.out.println("Abgerundete Noten: " + UniversityNotes.abgerundetenNoten(noten));
+        System.out.println("Maximale abgerundete Note: " + UniversityNotes.maxabgerundeteNote(noten));
+
     }
 }
