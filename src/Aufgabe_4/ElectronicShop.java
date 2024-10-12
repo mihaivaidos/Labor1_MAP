27251fe8c837fb737aeb0b7ab8235b0f08840f76
mihaivaidos <mihai@vaidos.com>
2024-10-12 17:03:21 +0300

    Implemented the last 2 methods for the 4th exercise

--- src/Aufgabe_4/ElectronicShop.java
+++ src/Aufgabe_4/ElectronicShop.java
@@ -1,5 +1,7 @@
 package Aufgabe_4;
 
+import java.util.Arrays;
+
 public class ElectronicShop {
 
     // 1
@@ -14,19 +16,45 @@ public class ElectronicShop {
     }
 
     // 2
-    public static int expensiveDevice(int []keyboards, int [] usbs) {
+    public static int expensiveDevice(int []keyboards, int []usb) {
         int expensiveDevice = keyboards[0];
         for(int keyboard : keyboards) {
             if(keyboard > expensiveDevice) {
                 expensiveDevice = keyboard;
             }
         }
-        for (int usb : usbs) {
-            if (usb > expensiveDevice) {
-                expensiveDevice = usb;
+        for (int u : usb) {
+            if (u > expensiveDevice) {
+                expensiveDevice = u;
             }
         }
         return expensiveDevice;
+    }
+
+    // 3
+    public static int affordExpensiveUSB(int []usb, int budget) {
+        Arrays.sort(usb);
+        for(int i = usb.length - 1; i >= 0; i--) {
+            if(usb[i] <= budget) {
+                return usb[i];
+            }
+        }
+        return -1; // Can't afford any USB
+    }
+
+    // 4
+    public static int spendMaxBoth(int []keyboards, int []usb, int budget) {
+        Arrays.sort(keyboards);
+        Arrays.sort(usb);
+        for(int i = keyboards.length - 1; i >= 0; i--) {
+            for(int j = usb.length - 1; j >= 0; j--) {
+                int sum = keyboards[i] + usb[j];
+                if(sum <= budget) {
+                    return sum;
+                }
+            }
+        }
+        return -1; // Can't buy both with that budget
     }
 
 }
